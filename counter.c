795b4a2adb938c35387d44e7272bdfc632449efb
swaleha08 <82538159+swaleha08@users.noreply.github.com>
2021-04-15 05:54:22 +0400

    removed min and max fn since its already in the function

--- counter.c
+++ counter.c
@@ -1,23 +1,8 @@
-/*******************************************************************************
-* 
-* Purpose: Assignment #3 solution.
-*
-* Author: Kevin Browne
-*
-*******************************************************************************/
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
 
-<<<<<<< HEAD
 #define COUNT_SIZE 26
-
-int main()
-{
-  char buffer[1024], input, curchar;
-  int i = 0, count[COUNT_SIZE];
-  int other = 0;
-=======
 #define BUFFER_SIZE 1024
 
 int max(int count[]);
@@ -26,9 +11,9 @@ int min(int count[]);
 int main()
 {
   char buffer[BUFFER_SIZE], input, curchar;
-  int i = 0, count[26];
+  int i = 0, count[COUNT_SIZE];
   int spaces = 0;
->>>>>>> origin/feature
+  int other = 0;
 
   // request and read in the string from the user
   printf("Enter text for analysis: ");
@@ -49,11 +34,8 @@ int main()
   for (i = 0; i < strlen(buffer); i++) {
     curchar = toupper(buffer[i]);
     if (curchar >= 65 && curchar <= 90) count[curchar - 65]++;
-<<<<<<< HEAD
     else other++;
-=======
     if (curchar == ' ') spaces++;
->>>>>>> origin/feature
   }
 
   // Create the letter analysis table
@@ -65,38 +47,11 @@ int main()
                                count[i],
                                (((float) count[i]) / strlen(buffer)) * 100);
   }
-<<<<<<< HEAD
   // Output the number of other characters
   printf("%-10s%-15d%-15.2f\n","Other",
                               other,
                               (((float) count[i]) / strlen(buffer)) * 100);
 
-  // Find the max and min occuring character in the string, in particular the
-  // position in the count array of each character
-  int max, min, max_pos, min_pos;
-  max = min = count[0];
-  min_pos = max_pos = 0;
-  for (i = 0; i < COUNT_SIZE; i++) {
-    if (count[i] < min)
-    {
-      min_pos = i;
-      min = count[i];
-    }
-    if (count[i] > max)
-    {
-      max_pos = i;
-      max = count[i];
-    }
-  }
-
-  // Output the max and min occuring letter, again using 65 as an offset to
-  // output the character character given ASCII A-Z range from 65-90
-  printf("\nThe most frequently occurring letter is %c.\n", max_pos + 65);
-  printf("The least frequently occurring letter is %c.\n", min_pos + 65);
-
-  return 0;
-}
-=======
   printf("\nTotal spaces: %d\n", spaces);
 
 
@@ -146,4 +101,3 @@ int min(int count[])
   }  
   return min_pos;
 }
->>>>>>> origin/feature
